2596952d656c96a0cceca34601a86cc7f322c325
Hans Verschuren <hansverschuren@MacBook-Pro-van-Hans.local>
2025-02-25 17:00:27 +0100

    Aanpassingen voor Render deployment

--- server.js
+++ server.js
@@ -73,8 +73,8 @@ app.get('/api/stats', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${port}`);
 });
 
 const { Parser } = require('json2csv');
